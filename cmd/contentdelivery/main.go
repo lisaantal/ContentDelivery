69d26ee2a420f17a987bec886953aedd2c3f3b74
Lisa <78325361+lisaantal@users.noreply.github.com>
2025-10-15 19:47:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/contentdelivery/main.go
@@ -0,0 +1,23 @@
+// cmd/contentdelivery/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "contentdelivery/internal/contentdelivery"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := contentdelivery.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
